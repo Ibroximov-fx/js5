c98a4f8f051d521da431666d50d14b1ba589d59e
ibroximov-fx <ibrohimovahmad91@gmail.com>
2025-05-29 09:13:36 -0700

    js5

---
+++ main.js
@@ -0,0 +1,119 @@
+// let number =[]
+// number.push(1, 2, 3, 4, 5);
+//
+// let books = ["kitob"]
+// books.push("yangi kitob")
+//
+// let names = ["sardor"]
+// names.push("raisboy")
+// console.log(names, books, number);
+
+
+
+
+
+
+// let fruits = ["olma", "anor", "olcha", "gilos"]
+// console.log(fruits[fruits.length - 1])
+// fruits.pop()
+// console.log(fruits)
+//
+// let number = [1, 2, 3, 4, 5]
+// console.log(number[number.length - 1], number[number.length - 2])
+// number.pop()
+// number.pop()
+// console.log(number)
+
+
+
+
+
+
+// let navbat = ["sanjar", "sardor", "ahmad", "jasur"]
+// navbat.shift()
+// console.log(navbat)
+//
+// let number = [1, 2, 3, 4, 5]
+// number.shift()
+// number.shift()
+// number.shift()
+// number.shift()
+// number.shift()
+// console.log(number)
+
+
+
+
+
+
+
+// let names = []
+// names.unshift("sanjar","sardor","jasur")
+// console.log(names, names.length)
+//
+// let todo = ["un", "suv", "choy"]
+// todo.unshift("nos")
+// console.log(todo, todo.length)
+
+
+
+
+
+
+
+// let number = [1, 2, 3, 4, 5]
+// console.log(number.length)
+// let white = []
+// console.log(white.length)
+// let symbol = ["1", "2", "3", "4", "5"]
+// console.log(symbol.length)
+
+
+
+
+
+
+// let a= ["birbalo"]
+// let b = ["birbalo2"]
+// let c = a.concat(b)
+// console.log(c)
+//
+// let sticer= ["smile", "photo"]
+// let phone = ["nokia"]
+// let people = ["shermat", "toshmat"]
+// let all = sticer.concat(phone, people)
+// console.log(all)
+//
+// let pust = []
+// let all2 = pust.concat(10, 20, 30, 40)
+// console.log(all2)
+
+
+
+
+
+
+
+// let number = [10, 20, 30, 40, 50]
+// console.log(number.slice(1, 4))
+//
+// let symbl = ["a", "b",  "c", "d", "e"]
+// console.log(symbl.slice(3, 5))
+//
+// chala 3 savol yuq
+
+
+
+
+
+
+
+// 13 SPLICE QILISH KERAK
+
+
+
+
+
+
+
+// 18 JOIN QILISH KERAK
